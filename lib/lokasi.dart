b0b3ea3a68ea53eff1a34d97f61173bd43d376cc
Yukari <topexgames@yahoo.com>
2019-08-14 11:47:56 +0700

    fix manifest, lokasi & readme

--- lib/lokasi.dart
+++ lib/lokasi.dart
@@ -13,7 +13,7 @@ class _LokasiState extends State<Lokasi> {
   Completer<GoogleMapController> _controller = Completer();
   LatLng latLng = LatLng(Kontak.lat, Kontak.lng);
   Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
-  double zoom = 16.0;
+  double _zoom = 19.0;
 
   @override
   void initState() {
@@ -25,64 +25,89 @@ class _LokasiState extends State<Lokasi> {
       infoWindow: InfoWindow(title: Kontak.nama, snippet:  Kontak.alamat),
       onTap: () {},
     );
-
     markers[markerId] = marker;
-
     WidgetsBinding.instance.addPostFrameCallback((_) { // when widget built
-      //_refreshIndicatorKey[_tabPosition].currentState.show();
-      animateZoom();
+      _animateZoom();
     });
+  }
+
+  Future<void> _animateZoom() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newLatLngZoom(latLng, _zoom));
+  }
+  _zoomIn() {
+    if (_zoom < 20.0) setState(() => _zoom += 1.0);
+    _animateZoom();
+  }
+  _zoomOut() {
+    if (_zoom > 10.0) setState(() => _zoom -= 1.0);
+    _animateZoom();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      /* body: GoogleMap(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: HSLColor.fromColor(Colors.blue).withLightness(0.85).toColor(),
+        titleSpacing: 0.0,
+        title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[
+          InkWell(
+            onTap: () => Navigator.of(context).pop(true),
+            child: Container(
+              padding: EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 20),
+              child: Row(children: <Widget>[
+                Icon(Icons.chevron_left, color: Colors.black87,),
+                SizedBox(width: 8,),
+                Text("Peta Lokasi", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+              ],),
+            ),
+          ),
+          IconButton(
+            color: Colors.black87,
+            icon: Icon(Icons.person_pin),
+            tooltip: "Street View",
+            onPressed: () {}, //TODO launch streetview
+          ),
+        ],),
+      ),
+      body: GoogleMap(
         mapType: MapType.normal,
         markers: Set<Marker>.of(markers.values),
         initialCameraPosition: CameraPosition(
           target: latLng,
-          zoom: zoom,
+          zoom: _zoom,
           bearing: 0,
           tilt: 0,
         ),
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
-      ), */
-      body: Container(),
-      floatingActionButton: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: _zoomIn,
-            heroTag: "_fabZoomIn",
-            child: Icon(Icons.zoom_in),
-            backgroundColor: Colors.blue,
-            mini: true,
-          ),
-          SizedBox(height: 6,),
-          FloatingActionButton(
-            onPressed: _zoomOut,
-            heroTag: "_fabZoomOut",
-            child: Icon(Icons.zoom_out),
-            backgroundColor: Colors.blue,
-            mini: true,
-          ),
-        ],
+      ),
+      floatingActionButton: Container(
+        margin: EdgeInsets.only(bottom: 50),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: _zoomIn,
+              heroTag: "_fabZoomIn",
+              child: Icon(Icons.zoom_in, color: Colors.white,),
+              backgroundColor: Colors.grey[700],
+              mini: true,
+            ),
+            //SizedBox(height: 8,),
+            FloatingActionButton(
+              onPressed: _zoomOut,
+              heroTag: "_fabZoomOut",
+              child: Icon(Icons.zoom_out, color: Colors.white,),
+              backgroundColor: Colors.grey[700],
+              mini: true,
+            ),
+          ],
+        ),
       ),
     );
-  }
-
-  Future<void> animateZoom() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newLatLngZoom(latLng, zoom));
-  }
-  _zoomIn() {
-    if (zoom < 20.0) setState(() => zoom += 1.0);
-  }
-  _zoomOut() {
-    if (zoom > 10.0) setState(() => zoom -= 1.0);
   }
 }
